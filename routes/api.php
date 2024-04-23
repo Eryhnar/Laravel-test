9d2459d55cb9c6b5f60d5cebe21e301089f936fd
pfernandez <pedrofernbel@gmail.com>
2024-04-23 11:51:01 +0200

    feat: added basic routes for tasks.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/tasks', function () {
+    return "get all tasks";
+});
+
+Route::post('/tasks', function () {
+    return "create a task";
+});
+
+Route::get('/tasks/{id}', function ($id) {
+    return "get task with id $id";
+});
+
+Route::put('/tasks/{id}', function ($id) {
+    return "update task with id $id";
+});
+
+Route::delete('/tasks/{id}', function ($id) {
+    return "delete task with id $id";
+});
